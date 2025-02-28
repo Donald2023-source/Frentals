0ad35d17c534da507fbdbd42925d62b9d9758d0f
Donald2023-source <donalddyusuf@gmail.com>
2025-02-28 05:58:52 -0800

    Hero

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -15,22 +15,26 @@ const Navbar = () => {
     const path = usePathname()
     console.log(path)
   return (
-    <div className='absolute top-0  left-0 w-full bg-gray-800 text-white p-2 flex justify-between'>
-      <div>
-        <Image className='h-16 w-16 rounded-full' src={logo} alt='logo' />
-      </div>
-      <div>
-        <ul className='flex space-x-11'>
-            {navItems.map((item, idx) =>(
-                <li className={path ===item.link ? 'font-bold cursor-pointer transition-all' : 'text-gray-400 hover:text-white cursor-pointer transition-all'}>{item.name}</li>
-            ))}
-        </ul>
+    <div>
+        <div className='absolute top-0 left-0 w-full  text-white py-2 px-5 items-center flex justify-between'>
+        <div>
+            <Image className='h-16 w-16 rounded-full' src={logo} alt='logo' />
+        </div>
+        <div className='flex items-center space-x-4'>
+            <ul className='flex space-x-11'>
+                {navItems.map((item, idx) =>(
+                    <li className={path ===item.link ? 'font-bold cursor-pointer transition-all' : 'text-gray-400 hover:text-white cursor-pointer transition-all'}>{item.name}</li>
+                ))}
+            </ul>
+        </div>
 
         <div className='flex space-x-4'>
-            <button className='border py-2 px-5 rounded-lg'>Sign In</button>
-            <button className='border py-2 px-5 rounded-lg'>Sign up</button>
+                <button className='border py-2 px-5 cursor-pointer rounded-lg'>Sign In</button>
+                <button className='py-2 hover:scale-105 transition-all cursor-pointer px-5 rounded-lg bg-[#3E803E]'>Sign up</button>
+            </div>
         </div>
-      </div>
+
+        <div className='absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10' />
     </div>
   )
 }

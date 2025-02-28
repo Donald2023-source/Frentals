b921024a9caed3c0c4190faa1d58d3b52631264c
Donald2023-source <donalddyusuf@gmail.com>
2025-02-28 05:23:42 -0800

    Navbar

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    const navItems = [
+        { name: 'Home', link: '/' }, 
+        { name: 'Services', link: '/services' }, 
+        { name: 'About Us', link: '/about' }, 
+        { name: 'Contact Us', link: '/contact' }, 
+        { name: 'FAQ', link: '/faq' }, 
+    ]
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar

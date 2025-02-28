9bc4faf869780ad16f075bab3e68b0a29436d16e
Donald2023-source <donalddyusuf@gmail.com>
2025-02-28 05:19:23 -0800

    Hero.tsx

---
+++ app/Components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import hero from "@/assets/hero.jpeg";
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <div className="h-screen w-full">
+      <div className="h-3/4">
+        <Image className="w-full max-h-full object-cover" src={hero} alt="hero Image" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;

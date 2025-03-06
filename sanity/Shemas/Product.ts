a1440f0d391fabb5810e7dc6192aed16a1d6608a
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 23:02:55 -0800

    Product schema

--- sanity/Shemas/Product.ts
+++ sanity/Shemas/Product.ts
@@ -1,13 +1,55 @@
 import { defineField, defineType } from "sanity";
 
 export default defineType({
-    name: 'product',
-    title: 'Product',
-    type: 'string',
-    
-    fields: [
-        defineField({
-            name: ''
-        })
-    ]
-})
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+      description: "Enter the specification for this product",
+    }),
+
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: "category",
+      title: "Category",
+      type: "array",
+      of: [{ type: "reference", to: [{ type: "category" }] }],
+      validation: (rule) => rule.required()
+    }),
+
+    defineField({
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+        validation: (rule) => rule.required()
+    })
+  ],
+});

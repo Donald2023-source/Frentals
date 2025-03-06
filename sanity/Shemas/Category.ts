4617f038d09b10593ebcf3aaf35b980ed6c45330
Donald2023-source <donalddyusuf@gmail.com>
2025-03-05 22:39:50 -0800

    schema

---
+++ sanity/Shemas/Category.ts
@@ -0,0 +1,28 @@
+import { defineConfig, defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "category",
+  title: "Category",
+  type: "document",
+
+  fields: [
+    defineField({
+      name: "title",
+      title: "Category",
+      type: "string",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "string",
+    }),
+    defineField({
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+            hotspot: true
+        }
+    })
+  ],
+});

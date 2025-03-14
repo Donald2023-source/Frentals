4db962206c703fe9486fe50f20e40d4432f0d74b
Donald2023-source <donalddyusuf@gmail.com>
2025-03-14 13:20:25 -0700

    reset cart

--- app/(Pages)/cart/page.tsx
+++ app/(Pages)/cart/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 import React from "react";
-import { useSelector, UseSelector } from "react-redux";
+import { useDispatch, useSelector, UseSelector } from "react-redux";
 import { StoreState } from "@/types";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
+import { resetCart } from "@/redux/cartSlice";
 const page = () => {
   const { cart } = useSelector((state: StoreState) => state?.frentals);
   console.log(cart);
+
+  const dispatch = useDispatch();
+  const handleResetCart = () => {
+    dispatch(resetCart());
+    console.log("cart was reset");
+  };
 
   return (
     <div className="px-20 py-10">
@@ -23,6 +30,12 @@ const page = () => {
             </div>
           ))}
         </div>
+        <button
+          className="py-3 rounded-xl px-10 text-gray-200 bg-red-600 cursor-pointer"
+          onClick={handleResetCart}
+        >
+          Clear Cart
+        </button>
       </div>
     </div>
   );

8beaaad0b6b84eff330e30ad036e7595569561d3
Donald2023-source <donalddyusuf@gmail.com>
2025-03-19 15:49:31 +0100

    auth

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import img from "@/assets/SignImg.jpeg";
 import Image from "next/image";
 const page = () => {
+  // const [name, setName] = useState("")
+  // const [email, setEmail] = useState("")
+  // const [phoneNumber, setPhoneNumber] = useState("")
+  // const [password, setPassword] = useState("")
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="w-full max-h-screen h-full">
       <div className="h-screen relative w-full">
@@ -13,31 +31,53 @@ const page = () => {
           <div className="bg-white md:p-10 p-6 h-full rounded-2xl">
             <div className="flex flex-col gap-7">
               <h2 className="text-lg font-bold w-[60%] flex items-center text-center justify-center mx-auto">
-              Hi, Need an Equipment on Rent? Start Today!
+                Hi, Need an Equipment on Rent? Start Today!
               </h2>
 
               <form className="flex flex-col gap-7" action="">
                 <fieldset className="w-full flex items-center py-1 px-3 rounded-md border border-gray-300">
                   <legend className="px-3 text-sm text-gray-800">Name</legend>
-                    <input className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none" type="text" placeholder="James Audu" />
+                  <input
+                    className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none"
+                    type="text"
+                    placeholder="James Audu"
+                  />
                 </fieldset>
 
                 <fieldset className="w-full flex items-center py-1 px-3 rounded-md border border-gray-300">
                   <legend className="px-3 text-sm text-gray-800">Email</legend>
-                    <input className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none" type="text" placeholder="jamesaudu@gmail.com" />
+                  <input
+                    className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none"
+                    type="text"
+                    placeholder="jamesaudu@gmail.com"
+                  />
                 </fieldset>
 
                 <fieldset className="w-full flex items-center py-1 px-3 rounded-md border border-gray-300">
-                  <legend className="px-3 text-sm text-gray-800">Phone Number</legend>
-                    <input className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none" type="tel" placeholder="08123000238" />
+                  <legend className="px-3 text-sm text-gray-800">
+                    Phone Number
+                  </legend>
+                  <input
+                    className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none"
+                    type="tel"
+                    placeholder="08123000238"
+                  />
                 </fieldset>
 
                 <fieldset className="w-full flex items-center py-1 px-3 rounded-md border border-gray-300">
-                  <legend className="px-3 text-sm text-gray-800">Password</legend>
-                    <input className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none" type="password" placeholder="password" />
+                  <legend className="px-3 text-sm text-gray-800">
+                    Password
+                  </legend>
+                  <input
+                    className="focus:border-none py-1 tracking-wide text-sm h-full w-full flex items-center justify-center outline-none"
+                    type="password"
+                    placeholder="password"
+                  />
                 </fieldset>
 
-                <button className="py-2 my-4 px-10 bg-[#3E803E] text-white rounded-lg cursor-pointer hover:scale-105 hoverEffect">Submit</button>
+                <button className="py-2 my-4 px-10 bg-[#3E803E] text-white rounded-lg cursor-pointer hover:scale-105 hoverEffect">
+                  Submit
+                </button>
               </form>
             </div>
           </div>

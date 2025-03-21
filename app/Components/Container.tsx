48038a2a442958ff9905759e995f262b75a1a159
Donald2023-source <donalddyusuf@gmail.com>
2025-03-21 17:04:38 +0100

    dashboard

--- app/Components/Container.tsx
+++ app/Components/Container.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
-  return <div className="md:px-20 px-2">{children}</div>;
+  return <div className="md:px-26 px-2">{children}</div>;
 };
 
 export default Container;

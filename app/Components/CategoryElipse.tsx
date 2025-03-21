a88f6d0e6af4d75c2ddd6723b5163c469d3b534c
Donald2023-source <donalddyusuf@gmail.com>
2025-03-21 19:11:36 +0100

    dashboard

--- app/Components/CategoryElipse.tsx
+++ app/Components/CategoryElipse.tsx
@@ -4,7 +4,8 @@ import { Category } from "@/types";
 import { getCategory } from "@/lib/getData";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
-import { Link } from "lucide-react";
+import Link from "next/link";
+
 const CategoryElipse = () => {
   const [categories, setCategories] = React.useState<Category[]>([]);
 
@@ -17,10 +18,14 @@ const CategoryElipse = () => {
   }, []);
 
   return (
-    <div className="py-3">
-      <div>
+    <div className="py-3 w-full">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-5 w-full justify-items-stretch">
         {categories.map((item) => (
-          <Link href={`/category/${item?.title}`}>
+          <Link
+            key={item.title}
+            className="flex flex-col items-center gap-2 w-full"
+            href={`/category/${item?.title}`}
+          >
             <Image
               className="h-20 object-cover rounded-full w-20"
               width={200}
@@ -28,7 +33,7 @@ const CategoryElipse = () => {
               src={urlFor(item?.image).url()}
               alt={item?.title}
             />
-            <h2>{item?.title}</h2>
+            <h2 className="font-semibold">{item?.title}</h2>
           </Link>
         ))}
       </div>
